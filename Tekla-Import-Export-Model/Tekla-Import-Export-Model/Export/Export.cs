6baf9ed74839c202c691455c1d83ad2c973bf40d
merenk0ff <4646274@gmail.com>
2022-03-17 14:23:39 +0300

    prepare form

---
+++ Tekla-Import-Export-Model/Tekla-Import-Export-Model/Export/Export.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekla_Import_Export_Model.Export
+{
+    class Export
+    {
+    }
+}
